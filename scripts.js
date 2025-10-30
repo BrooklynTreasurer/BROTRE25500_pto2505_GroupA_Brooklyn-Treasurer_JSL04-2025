72a59ff2cb2b7caf3d99652b65b891a26972ee5e
Brooklyn <markmclean2005@gmail.com>
2025-10-30 23:28:09 +0200

    Added function to createTaskElement and Function to getTaskContainer by status

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+function createTaskElement(task) {
+  const taskDivElement = document.createElement('div');
+  taskDiv.className = 'task-div';
+  taskDiv.textContent = task.title;
+  taskDiv.dataset.id = task.id;
+  taskDivElement.addEventListener('click', () => openModal(task));
+  return taskDivElement;
+}
+
+function getTasksContainerByStatus(status) {
+   const column = document.querySelector(`.column-div[data-status="${status}"]`);
+   return column.querySelector('.tasks-container');
+}
